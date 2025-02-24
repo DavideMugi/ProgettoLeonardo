eb2a55f499543c5a1f505523f7af2459561a3f7c
DavideMugi <67126770+DavideMugi@users.noreply.github.com>
2025-02-24 03:27:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -131,7 +131,7 @@ if st.button("Trova", type="primary"):
     if len(universita) > 0:
       for i in range(len(universita)):
         st.write(universita[i])
-
+  st.divider()
   st.write(
     "Dati forniti da [OpenStreetMap](https://www.openstreetmap.org) sotto licenza [ODbL](https://opendatacommons.org/licenses/odbl/)."
   )
